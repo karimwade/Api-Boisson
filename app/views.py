152b25ea682e03dd47bcd71fba3aa172144f7c29
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-07-18 23:27:31 +0000

    Mise en place des mesthode de l'api rest avec la methode POST et GET

--- app/views.py
+++ app/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from .models import Boisson
 from .serializers import BoissonSerializer
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
-
+@api_view(['GET', 'POST'])
 def boisson_list(request):
-    boisson = Boisson.objects.all()
-    serializer=BoissonSerializer(boisson,many=True)
-    return JsonResponse({'boisson':serializer.data}, safe=False)
+    if request.method=='GET':
+        boisson = Boisson.objects.all()
+        serializer=BoissonSerializer(boisson,many=True)
+        return JsonResponse({'boisson':serializer.data})
 
+    if request.method=='POST':
+        serializer = BoissonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
