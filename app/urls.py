74b3c20c33932d6cca650b9ff868727a6619b9c5
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-07-20 13:28:28 +0000

    Ajout des metohodes PUT et DELETE pour la mise a jour et la supression

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('boisson',views.boisson_list,name="boisson")
+    path('boisson',views.boisson_list,name="boisson"),
+    path('boisson/<id>',views.boisson_detail)
 ]
--- app/views.py
+++ app/views.py
@@ -18,3 +18,25 @@ def boisson_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+@api_view(['GET', 'PUT','DELETE'])
+def boisson_detail(request,id):
+    try:
+       boisson = Boisson.objects.get(pk=id)
+    except Boisson.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=='GET':
+        serializer = BoissonSerializer(boisson)
+        return Response(serializer.data)
+
+    elif request.method=='PUT':
+        serializer = BoissonSerializer(boisson,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method=='DELETE':
+        boisson.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
